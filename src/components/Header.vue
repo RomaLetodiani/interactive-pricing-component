0f7c18147e2d1d0a7dea8ae00c250fd9c1280b8a
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-10 00:34:16 +0400

    Create Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+import circlesUrl from '../assets/images/pattern-circles.svg'
+</script>
+
+<template>
+  <div class="relative text-center">
+    <img
+      class="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+      :src="circlesUrl"
+      alt="Circles"
+    />
+    <span class="z-20 relative">
+      <h1 class="font-bold text-[clamp(18px,4vw,32px)]">Simple, Traffic-Based Pricing</h1>
+      <p class="text-[clamp(11px,2.5vw,15px)] text-[hsl(225,20%,60%)]">
+        Sign-up for our 30-day trial. No credit card required.
+      </p>
+    </span>
+  </div>
+</template>
