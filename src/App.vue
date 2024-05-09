a92e5cad16dde2d18b2652e25e3ec774fb412315
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-10 00:34:12 +0400

    Tailwlind

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <div
+    class="mainDiv w-full p-5 min-w-[320px] min-h-screen gap-20 flex flex-col justify-center items-center"
+  >
+    <Header />
+    <Card />
+  </div>
+</template>
