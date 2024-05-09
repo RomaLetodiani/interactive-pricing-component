fca457a4253e790f4809543b93b4ee080c866680
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-10 00:34:19 +0400

    Create Button.vue

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+// Define props using defineProps() function
+const props = defineProps({
+  text: String,
+})
+</script>
+
+<template>
+  <button class="bg-[hsl(227,35%,25%)] text-white rounded-full px-10 py-2">{{ props.text }}</button>
+</template>
